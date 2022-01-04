6735f670157ecbc959101aaa053a74364f888c3b
Filipe Casal <fcasal@users.noreply.github.com>
2022-01-04 18:05:41 +0000

    Remove false positives from storage_var and contract_interfaces.

--- rules/UnusedArgumentRule.py
+++ rules/UnusedArgumentRule.py
@@ -8,8 +8,6 @@ from rules.GenericRule import GenericRule
 class UnusedArgumentRule(GenericRule):
     """
     Check for unused arguments inside a function
-
-    TODO: remove false positives from functions within a @contract_interface
     """
 
     RULE_TEXT = "Unused arguments might indicate a misspelled variable use or unnecessary argument."
@@ -32,11 +30,27 @@ class UnusedArgumentRule(GenericRule):
             elif child.data == "code_block":
                 code_block = child
 
+            # ignore if the function is a @storage_var
+            elif child.data == "decorator_list":
+                for decorator in child.find_data("identifier_def"):
+                    # since this is a class attribute, there is no need to check argument usage because there is no code.
+                    if decorator.children[0] == "storage_var":
+                        return
+
         used_ids: Set[Token] = set()
         for code_child in code_block.find_data("identifier"):
             used_ids.add(code_child.children[0])
 
         unused_arguments = arguments - used_ids
+
+        # find if this is part of a @contract_interface
+        for struct in self.original_tree.find_data("code_element_struct"):
+            for child in struct.find_data("decorator_list"):
+                for decorator in child.find_data("identifier_def"):
+                    if decorator.children[0] == "contract_interface":
+                        if tree in struct.iter_subtrees():
+                            return
+
         for arg in unused_arguments:
             positions = (arg.line, arg.column, arg.end_line, arg.end_column)
             sarif = generic_sarif(
