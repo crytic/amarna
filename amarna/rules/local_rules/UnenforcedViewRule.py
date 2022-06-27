5dc1dda64c3a9bdc9adbd326134af12713562bcf
coolhill <8256597+coolhill@users.noreply.github.com>
2022-06-27 17:33:04 +0200

    black

--- amarna/rules/local_rules/UnenforcedViewRule.py
+++ amarna/rules/local_rules/UnenforcedViewRule.py
@@ -24,7 +24,7 @@ class UnenforcedViewRule(GenericRule):
             return
 
         for function_call in tree.find_data("function_call"):
-            
+
             for args in function_call.find_data("identifier"):
                 if len(args.children) > 1:
                     if str(args.children[1]) == "write":
