52cab843a509a79ccfe0223cd826c35f042ff4de
coolhill <8256597+coolhill@users.noreply.github.com>
2022-07-07 12:56:32 +0200

    Feature: Rule for state modification in @view functions (#42)

---
+++ amarna/rules/post_process_rules/UnenforcedViewRule.py
@@ -0,0 +1,71 @@
+from typing import Dict, List
+from amarna.Result import ResultMultiplePositions, result_multiple_positions
+from amarna.rules.GenericRule import GenericRule
+
+from amarna.rules.gatherer_rules.DeclaredFunctionsGatherer import (
+    DeclaredFunctionsGatherer,
+    FunctionType,
+)
+from amarna.rules.gatherer_rules.AllFunctionCallsGatherer import (
+    AllFunctionCallsGatherer,
+    FunctionCallType,
+)
+
+
+class UnenforcedViewRule(GenericRule):
+    """
+    Find state modifications in functions with @view decorator.
+    """
+
+    RULE_TEXT = (
+        "[This](0) function call modifies state but is called within [this](1) @view context."
+    )
+    RULE_NAME = "unenforced-view"
+
+    def run_rule(self, gathered_data: Dict) -> List[ResultMultiplePositions]:
+        declared_functions: List[FunctionType] = gathered_data[
+            DeclaredFunctionsGatherer.GATHERER_NAME
+        ]
+        function_calls: List[FunctionCallType] = gathered_data[
+            AllFunctionCallsGatherer.GATHERER_NAME
+        ]
+
+        results = []
+
+        def check_parents(call: FunctionCallType, original_call: FunctionCallType):
+            for func in declared_functions:
+
+                # if it is not the parent, skip it
+                if func.name != call.parent_function:
+                    continue
+
+                # if the parent is a view, add to result
+                if "view" in func.decorators:
+                    result = result_multiple_positions(
+                        [original_call.file_name, func.file_location],
+                        self.RULE_NAME,
+                        self.RULE_TEXT,
+                        [original_call.position, func.position],
+                    )
+                    if result.position_list not in (r.position_list for r in results):
+                        results.append(result)
+                    break
+
+                seen = [call.function_name]
+                # check if the parent is called somewhere else
+                for f in function_calls:
+                    if f.function_name != call.parent_function:
+                        continue
+                    if f.function_name not in seen:
+                        seen.append(f.function_name)
+                        check_parents(f, original_call)
+
+        for f in function_calls:
+            if (
+                f.tail_name == "write"
+                or f.tail_name == "emit"
+                or f.function_name == "storage_write"
+            ):
+                check_parents(f, f)
+
+        return results
---
+++ tests/state_modification_view_test/main.cairo
@@ -0,0 +1,26 @@
+@view
+func bad_get_nonce{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}() -> (nonce : felt):
+    let (user) = get_caller_address()
+    let (nonce) = user_nonces.read(user)
+    user_nonces.write(user, nonce + 1)
+    storage_write()
+    bar()
+    return (nonce)
+end
+
+func bar():
+    storage_write()
+    foo()
+    return()
+end
+
+func foo():
+    storage_write()
+    bro.emit() 
+    return()
+end
+
+@event
+func bro():
+    return()
+end
