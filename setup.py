d9623aa3ec613df9abc865f1bb3b70a6db6f2635
coolhill <8256597+coolhill@users.noreply.github.com>
2022-07-15 15:42:19 +0200

    Add repo metadata for PyPi  (#49)

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="amarna",
-    version="0.1",
+    version="0.1.2",
     description="Amarna is a static-analyzer for the Cairo programming language.",
+    url="https://github.com/crytic/amarna",
     author="Trail of Bits",
     license="AGPL-3.0",
     packages=find_packages(),
@@ -12,6 +16,8 @@ setup(
         "lark>=1.1.2",
         "pydot>=1.4.2",
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         "console_scripts": [
             "amarna=amarna.command_line:main",
